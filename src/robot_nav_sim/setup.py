4d21165018e18749751d40110b5c7ae3a2658943
Angelina Simon <angelina.m.simon@gmail.com>
2026-06-11 22:27:55 +0000

    Scaffold ROS 2 workspace + robot_nav_sim package

---
+++ src/robot_nav_sim/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+package_name = 'robot_nav_sim'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='angelina_m_simon',
+    maintainer_email='angelina_m_simon@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
